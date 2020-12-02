89eb2138a0b4a4ca5b081daf88754e122dae952b
Drew Harper <druharper@gmail.com>
2020-12-02 17:01:07 -0600

    @click toggle computed properties

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
 const app = Vue.createApp({
   data() {
     return {
+      url: 'http://drewharper.com',
       showBooks: true,
       books: [
-        { title: 'Name of the Wind', author: 'Fred Johnson' },
-        { title: 'The Way of Kings', author: 'Mildred Honeycutt' },
-        { title: 'The Final Empire', author: 'Mac Wilson' },
+        {
+          title: 'Name of the Wind',
+          author: 'Fred Johnson',
+          img: '1.jpg',
+          isFav: true,
+        },
+        {
+          title: 'The Way of Kings',
+          author: 'Mildred Honeycutt',
+          img: '2.jpg',
+          isFav: false,
+        },
+        {
+          title: 'The Final Empire',
+          author: 'Mac Wilson',
+          img: '3.jpg',
+          isFav: true,
+        },
       ],
       x: 0,
       y: 0,
     };
   },
   methods: {
+    toggleIsFav(book) {
+      console.log(book, 'book is fav');
+      book.isFav = !book.isFav;
+    },
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
     },
@@ -24,6 +44,11 @@ const app = Vue.createApp({
     handleMouseMove(e) {
       this.x = e.offsetX;
       this.y = e.offsetY;
+    },
+  },
+  computed: {
+    filteredBooks() {
+      return this.books.filter((book) => book.isFav);
     },
   },
 });
